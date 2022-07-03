851ba2191ac49e0ef444c7cd680dc20f071bc8a6
lubeso <lubeso@icloud.com>
2022-07-03 03:00:15 -0400

    Implement utility method and helper methods

--- src/utils/initElement.ts
+++ src/utils/initElement.ts
@@ -3,8 +3,8 @@ import type { ElementConfig } from '../@types/ElementConfig.js';
 
 /**
  * Apply the specified configuration to the element.
- * @param _element an element in the document.
- * @param _config the element configuration.
+ * @param element an element in the document.
+ * @param config the element configuration.
  */
 export function initElement<
     T extends Element,
@@ -12,67 +12,84 @@ export function initElement<
     P extends Node,
     C extends Node
 >(
-    _element: T,
-    _config: ElementConfig<T, A, P, C>
-) {
-    throw new Error('not implemented yet');
+    element: T,
+    config: Partial<ElementConfig<T, A, P, C>> = {}
+): void {
+    if (config.properties) {
+        setProperties(element, config.properties);
+    }
+    if (config.attributes) {
+        setAttributes(element, config.attributes)
+    }
+    if (config.parent) {
+        appendToParent(element, config.parent);
+    }
+    if (config.children) {
+        appendChildren(element, config.children);
+    }
 }
 
 /**
  * Apply the specified attributes to the element.
- * @param _element an element in the document.
- * @param _attributes the attributes to apply.
+ * @param element an element in the document.
+ * @param attributes the attributes to apply.
  */
-function _setAttributes<
+function setAttributes<
     T extends Element,
     A extends ElementAttributes
 >(
-    _element: T,
-    _attributes: A,
+    element: T,
+    attributes: Partial<A>,
 ): void {
-    throw new Error('not implemented yet');
+    for (const [key, value] of Object.entries(attributes)) {
+        element.setAttribute(key, value);
+    }
 }
 
 /**
  * Apply the specified properties for the element.
- * @param _element an element in the document.
- * @param _properties the attributes to apply.
+ * @param element an element in the document.
+ * @param properties the attributes to apply.
  */
-function _setProperties<
+function setProperties<
     T extends Element
 >(
-    _element: T,
-    _properties: Partial<T>
+    element: T,
+    properties: Partial<T>
 ): void {
-    throw new Error('not implemented yet');
+    for (const [key, value] of Object.entries(properties)) {
+        (element as any)[key] = value;
+    }
 }
 
 /**
  * Append the element to the specified parent.
- * @param _element an element in the document.
- * @param _parent the parent to append to.
+ * @param element an element in the document.
+ * @param parent the parent to append to.
  */
-function _appendToParent<
+function appendToParent<
     T extends Element,
     P extends Node
 >(
-    _element: T,
-    _parent: P,
+    element: T,
+    parent: P,
 ): void {
-    throw new Error('not implemented yet');
+    parent.appendChild(element);
 }
 
 /**
  * Append the specified children to the element.
- * @param _element an element in the document.
- * @param _children the children to append.
+ * @param element an element in the document.
+ * @param children the children to append.
  */
-function _appendChildren<
+function appendChildren<
     T extends Element,
     C extends Node
 >(
-    _element: T,
-    _children: C[],
+    element: T,
+    children: C[],
 ): void {
-    throw new Error('not implemented yet');
+    for (const child of children) {
+        element.append(child)
+    }
 }
