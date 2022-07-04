2ccd26363a40e805ede31324586a0d282e3578c0
lubeso <lubeso@icloud.com>
2022-07-04 01:42:37 -0400

    Revise implementation of Renderable interface

--- src/Viewer.ts
+++ src/Viewer.ts
@@ -1,3 +1,7 @@
+import {
+    SVGElementConfig as RenderConfig,
+    SVGElementAttributes,
+} from './types.js';
 import { Renderable } from './Renderable.js';
 import { Ellipse } from './Ellipse.js';
 
@@ -20,35 +24,46 @@ export interface ViewerConfig {
 /**
  * Interactive widget for visualizing three-ellipses-arcs.
  */
-export class Viewer extends Renderable<SVGSVGElement> {
+export class Viewer<
+    A extends SVGElementAttributes,
+    P extends Node,
+    C extends Node
+> extends Renderable<SVGSVGElement> {
 
     /**
      * Viewer configuration settings
      */
-    private readonly config: ViewerConfig;
+    private readonly viewerConfig: ViewerConfig;
+    /**
+     * Viewer rendering settings
+     */
+    private readonly renderConfig: Partial<RenderConfig<SVGSVGElement, A, P, C>>;
     /**
      * Ellipses to be rendered.
      */
-    private readonly ellipses: Ellipse[] = [];
+    private readonly ellipses: Ellipse<SVGElementAttributes, Node, Node>[] = [];
 
     /**
      * Create a new Viewer instance.
-     * @param config viewer configuration
+     * @param viewerConfig viewer configuration
+     * @param renderConfig render configuration
      * @param pointer corresponding DOM element
      */
     protected constructor (
-        config: ViewerConfig,
+        viewerConfig: ViewerConfig,
+        renderConfig: Partial<RenderConfig<SVGSVGElement, A, P, C>>,
         pointer: SVGSVGElement
     ) {
         super(pointer);
-        this.config = Object.assign({}, config);
+        this.viewerConfig = Object.assign({}, viewerConfig);
+        this.renderConfig = Object.assign({}, renderConfig);
     }
 
     /**
      * Return the `i`-th ellipse.
      * @param i numerical index of desired ellipse.
      */
-    public getEllipse(i: number): Ellipse {
+    public getEllipse(i: number): Ellipse<SVGElementAttributes, Node, Node> {
         if (i >= 0 && i < this.ellipses.length) {
             return this.ellipses[i];
         } else {
@@ -63,30 +78,45 @@ export class Viewer extends Renderable<SVGSVGElement> {
      * Add a new ellipse.
      * @param ellipse the ellipse to add.
      */
-    public addEllipse(ellipse: Ellipse): Viewer {
+    public addEllipse(
+        ellipse: Ellipse<SVGElementAttributes, Node, Node>
+    ): Viewer<A, P, C> {
         this.ellipses.push(ellipse);
         return this;
     }
 
     public get width(): number {
-        return this.config.width;
+        return this.viewerConfig.width;
     }
 
     public get height(): number {
-        return this.config.height;
+        return this.viewerConfig.height;
     }
 
     /**
      * Create a new viewer.
-     * @param config viewer configuration.
+     * @param viewerConfig viewer configuration.
+     * @param renderConfig render configuration.
      */
-    public static make(config: ViewerConfig): Viewer {
+    public static make<
+        A extends SVGElementAttributes,
+        P extends Node,
+        C extends Node
+    >(
+        viewerConfig: ViewerConfig,
+        renderConfig: Partial<RenderConfig<SVGSVGElement, A, P, C>> = {}
+    ): Viewer<A, P, C> {
         const pointer = SVG.createElement('svg', {
+            properties: renderConfig.properties,
             attributes: {
-                viewBox: `0 0 ${config.width} ${config.height}`,
-            }
+                ...renderConfig.attributes,
+                width: `${viewerConfig.width}`,
+                height: `${viewerConfig.height}`,
+            },
+            parent: renderConfig.parent,
+            children: renderConfig.children,
         });
-        return new Viewer(config, pointer)
+        return new Viewer(viewerConfig, renderConfig, pointer)
     }
 
 }
