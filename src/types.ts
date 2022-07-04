f7d0b038e28c490b9ae78a7b39c55635df358eb2
lubeso <lubeso@icloud.com>
2022-07-04 00:47:06 -0400

    Add types definition file

---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export type { ElementAttributes } from './@types/ElementAttributes.js';
+export type { ElementConfig } from './@types/ElementConfig.js';
+export type { HTMLElementAttributes, HTMLElementConfig} from './@types/HTML.js';
+export type { SVGElementAttributes, SVGElementConfig } from './@types/SVG.js';
+export type { Point2D } from './@types/Point2D.js';
+
+export type { ViewerConfig } from './Viewer.js';
+export type { EllipseConfig } from './Ellipse.js';
