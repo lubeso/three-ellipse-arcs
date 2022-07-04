a14f57531672027ea6ee2a5671f02d406db1d03b
lubeso <lubeso@icloud.com>
2022-07-04 00:47:15 -0400

    Define module exports

---
+++ src/mod.ts
@@ -0,0 +1,5 @@
+export * from './types.js';
+export * from './utils.js';
+export { Renderable } from './Renderable.js';
+export { Ellipse } from './Ellipse.js'
+export { Viewer } from './Viewer.js';
