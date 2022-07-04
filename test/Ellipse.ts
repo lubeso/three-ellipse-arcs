8c23744f41c08e63a0279bd17a0a6e4104a5a430
lubeso <lubeso@icloud.com>
2022-07-04 00:22:44 -0400

    Fix typo

--- test/Ellipse.ts
+++ test/Ellipse.ts
@@ -13,15 +13,15 @@ describe('Ellipse', () => {
             center: { x: 0, y: 0 },
             rx: 1,
             ry: 2,
-            rotation: 45
+            rotation: 45,
         };
         it('returns: expected output', () => {
             const ellipse = Ellipse.make(ellipseConfig);
             const tests = [
                 { actual: ellipse.center, expected: ellipseConfig.center },
                 { actual: ellipse.rx, expected: ellipseConfig.rx },
-                { actual: ellipse.ry, expected: ellipseConfig.ry },
-                { actual: ellipse.rotation, expect: ellipse.rotation }
+                { actual: ellipse.ry, expected: ellipseConfig.ry }
+                { actual: ellipse.rotation, expected: ellipseConfig.rotation },
             ];
             for (const { actual, expected } of tests) {
                 expect(actual).to.be.equal(expected);
