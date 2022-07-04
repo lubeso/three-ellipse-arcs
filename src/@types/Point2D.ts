fc9c0e1fb06eca5912393a484e5a9ac8baf6775c
lubeso <lubeso@icloud.com>
2022-07-03 23:49:12 -0400

    Add documentation

--- src/@types/Point2D.ts
+++ src/@types/Point2D.ts
@@ -1,4 +1,13 @@
+/**
+ * Type representing a point in two-dimensional Cartesian space.
+ */
 export interface Point2D {
+    /**
+     * The x-coordinate.
+     */
     x: number;
+    /**
+     * The y-coordinate.
+     */
     y: number;
 }
