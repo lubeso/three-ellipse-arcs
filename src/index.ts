f06a6a23978be85d8884cc343f40cc26de991a27
lubeso <lubeso@icloud.com>
2022-07-04 01:42:45 -0400

    Revise app mains cript

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,68 @@
+import type { ViewerConfig, EllipseConfig } from './mod.js';
+import { Viewer, Ellipse } from './mod.js';
+
+function getAppDimensions(): { width: number, height: number } {
+    const app = document.body.querySelector('.app')!;
+    const bbox = app.getBoundingClientRect();
+    return {
+        width: bbox.width,
+        height: bbox.height,
+    };
+}
+
+const appDimensions = getAppDimensions();
+
+const viewerConfig: ViewerConfig = {
+    width: appDimensions.width,
+    height: appDimensions.height,
+};
+
+const defaultEllipseConfig: EllipseConfig = {
+    center: {
+        x: viewerConfig.width / 2,
+        y: viewerConfig.height / 2,
+    },
+    rx: viewerConfig.width / 16,
+    ry: viewerConfig.height / 4,
+    rotation: 0
+};
+
+const ellipseConfigs: EllipseConfig[] = [
+    {
+        ...defaultEllipseConfig,
+        center: { 
+            x: viewerConfig.width / 2,
+            y: viewerConfig.height / 2,
+        },
+    },
+    {
+        ...defaultEllipseConfig,
+        center: { 
+            x: viewerConfig.width / 6,
+            y: 2 * viewerConfig.height / 3,
+        },
+    },
+    {
+        ...defaultEllipseConfig,
+        center: { 
+            x: 5 * viewerConfig.width / 6,
+            y: 2 * viewerConfig.height / 3,
+        },
+    },
+];
+
 export function main(): void {
-    console.log('hello world!');
+    const viewer = Viewer.make(
+        viewerConfig, 
+        { parent: document.body.querySelector('.app > .viewer')! }
+    );
+    const ellipses = ellipseConfigs
+        .map(ellipseConfig => {
+            return Ellipse.make(
+                ellipseConfig,
+                { parent: viewer.pointer }
+            );
+        });
 }
 
 main();
